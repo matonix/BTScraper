039e1ef9958627135e5c9ad115ef7544edeb4567
matonix <springnaraosaka@gmail.com>
2016-09-23 15:20:27 +0900

    :art: restructure the python scraper

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,17 @@
 module Main where
 
--- import System.Environment (getArgs)
+import System.Environment (getArgs)
 import Python
 
-url = "http://bugs.python.org/issue24022"
+-- url = "http://bugs.python.org/issue24022"
+
+usage = putStrLn "Usage: cmd filename"
 
 main :: IO ()
-main = scrapePythonURL url >>= print
--- main = do
---   args <- getArgs
---   if length args /= 1
---     then usage = putStrLn "Usage: cmd filename"
---     else do
---       let filename = head args
+main = do
+  args <- getArgs
+  if length args /= 1
+    then usage
+    else do
+      let url = head args
+      scrapePythonURL url >>= print
--- src/Python.hs
+++ src/Python.hs
@@ -15,56 +15,37 @@ import qualified Stats as S
 import Stats (Stats)
 import Text.HTML.Scalpel hiding (URL)
 
-data PythonBTS = PythonBTS
-  { pythonPriority :: [String]
+type URL = String
+type PythonFieldSet = [String]
+type PythonHistory = [String]
+data PythonIssue = PythonIssue
+  { pythonFieldSets :: [PythonFieldSet]
   , pythonHistories :: [PythonHistory]
   } deriving Show
 
-data PythonHistory = PythonHistory
-  { date    :: String
-  , user    :: String
-  , action  :: String
-  , args    :: String
-  } deriving Show
+scrapePythonURL :: URL -> IO (Maybe Stats)
+scrapePythonURL url = fmap parsePythonIssue <$> scrapeURL url issue
 
-scrapePythonURL :: String -> IO (Maybe Stats)
-scrapePythonURL url = fmap parsePythonBTS <$> scrapeURL url bts
+issue :: Scraper String PythonIssue
+issue = PythonIssue <$> fieldSets <*> histories
 
-bts :: Scraper String PythonBTS
-bts = PythonBTS <$> priority <*> histories
-
-priority :: Scraper String [String]
-priority = chroots ("fieldset" // "table") priority'
-  where
-    priority' :: Scraper String String
-    priority' = do
-      cols <- mfilter ((==4) . length) . texts $ "tr"
-      return $ cols !! 3
-
+fieldSets :: Scraper String [PythonFieldSet]
+fieldSets = chroots ("fieldset" // "table") $ texts "td"
 
 histories :: Scraper String [PythonHistory]
-histories = chroots ("table" @: [hasClass "table-striped"] // "tr") history
-  where
-    history :: Scraper String PythonHistory
-    history = do
-      tds <- texts "td"
-      return PythonHistory
-        { date    = tds !! 0
-        , user    = tds !! 1
-        , action  = tds !! 2
-        , args    = tds !! 3
-        }
+histories = chroots ("table" @: [hasClass "table-striped"] // "tr") $ texts "td"
 
-parsePythonBTS :: PythonBTS -> Stats
-parsePythonBTS bts = S.Stats
+parsePythonIssue :: PythonIssue -> Stats
+parsePythonIssue issue = S.Stats
   { S.period = udtSeconds $ diffUnixTime new old
-  , S.priority = (!! 1) . words . head $ pythonPriority bts
-  , S.reopen = pred . length . filter (isPrefixOf "-> closed") . tails $ concatMap args hists
+  , S.priority = pythonFieldSets issue !! 1 !! 6
+  , S.reopen = pred . length . filter (isInfixOf "-> closed") $ map (!! 3) hists
   }
     where
-      hists = pythonHistories bts :: [PythonHistory]
-      new = parsePythonTime . pack . date . head $ hists
-      old = parsePythonTime . pack . date . last $ hists
+      hists = pythonHistories issue :: [PythonHistory]
+      time = parsePythonTime . pack . (!! 0)
+      new = time $ head hists
+      old = time $ last hists
 
 parsePythonTime :: ByteString -> UnixTime
 parsePythonTime = parseUnixTime . pack $ "%Y-%m-%d&nbsp;%H:%M:%S"
