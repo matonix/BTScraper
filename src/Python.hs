f22fe494d1b768bc2931d5ecd2e9255fcff01860
matonix <springnaraosaka@gmail.com>
2016-09-24 04:49:07 +0900

    :sparkles: run scraper with csv

--- src/Python.hs
+++ src/Python.hs
@@ -1,27 +1,35 @@
 module Python
-  (
-  scrapePythonURL,
+  ( scrapePythonURL
+  , scrapePythonCsv
+  , parseCsv
   ) where
 
 import Control.Monad
-import Debug.Trace
-import Data.Maybe
+import Data.Csv
+import Data.Maybe (mapMaybe, fromJust)
 import Data.UnixTime
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy.Char8 as BL
+import qualified Data.Vector as V
+import Debug.Trace
 import Foreign.C.Types
-import qualified Stats as S
-import Stats (Stats)
+import Stats
 import Text.HTML.Scalpel hiding (URL)
---
--- type CSV = [[ByteString]]
---
--- parseCSV :: FilePath -> IO CSV
--- parseCSV ::
+
+scrapePythonCsv :: FilePath -> IO [Maybe Stats]
+scrapePythonCsv csvFile = do
+  Right csv <- parseCsv csvFile
+  mapM scrapePythonURL $ getIssueNums csv
+
+parseCsv :: FilePath -> IO (Either String Csv)
+parseCsv csvFile = decode NoHeader <$> BL.readFile csvFile
+
+getIssueNums :: Csv -> [Int]
+getIssueNums = map fst . mapMaybe (BS.readInt . (V.! 1)) . V.toList
 
 -- for scraping python issue
 
-type URL = String
 type PythonFieldSet = [ByteString]
 type PythonHistory = [ByteString]
 data PythonIssue = PythonIssue
@@ -29,8 +37,11 @@ data PythonIssue = PythonIssue
   , pythonHistories :: [PythonHistory]
   } deriving Show
 
-scrapePythonURL :: URL -> IO (Maybe Stats)
-scrapePythonURL url = fmap parsePythonIssue <$> scrapeURL url issue
+scrapePythonURL :: Int -> IO (Maybe Stats)
+scrapePythonURL issueNum =
+  fmap (parsePythonIssue issueNum) <$> scrapeURL url issue
+  where
+    url = "http://bugs.python.org/issue" ++ show issueNum
 
 issue :: Scraper ByteString PythonIssue
 issue = PythonIssue <$> fieldSets <*> histories
@@ -41,11 +52,12 @@ fieldSets = chroots ("fieldset" // "table") $ texts "td"
 histories :: Scraper ByteString [PythonHistory]
 histories = chroots ("table" @: [hasClass "table-striped"] // "tr") $ texts "td"
 
-parsePythonIssue :: PythonIssue -> Stats
-parsePythonIssue issue = S.Stats
-  { S.period = udtSeconds $ diffUnixTime new old
-  , S.priority = pythonFieldSets issue !! 1 !! 6
-  , S.reopen = pred . length . filter (BS.isInfixOf $ BS.pack "-> closed") $ map (!! 3) hists
+parsePythonIssue :: Int -> PythonIssue -> Stats
+parsePythonIssue inum issue = Stats
+  { issueNum = inum
+  , period = udtSeconds $ diffUnixTime new old
+  , priority = pythonFieldSets issue !! 1 !! 6
+  , reopen = pred . length . filter (BS.isInfixOf $ BS.pack "-> closed") $ map (!! 3) hists
   }
     where
       hists = pythonHistories issue :: [PythonHistory]
