a9524d77c5d73bd1e0e376e60caea7ddb3fd74cb
matonix <automatonix@gmail.com>
2016-09-24 17:50:49 +0900

    :raceharse: cache scraped stats

--- src/Python.hs
+++ src/Python.hs
@@ -1,29 +1,28 @@
 module Python
   ( scrapePythonURL
-  , scrapePythonCsv
-  , parseCsv
+  , scrapePythonCSV
   ) where
 
-import Control.Monad
-import Data.Csv
-import Data.Maybe (mapMaybe, fromJust)
-import Data.UnixTime
-import Data.ByteString (ByteString)
-import qualified Data.ByteString.Char8 as BS
+import           Control.Monad
+import           Data.ByteString            (ByteString)
+import qualified Data.ByteString.Char8      as BS
 import qualified Data.ByteString.Lazy.Char8 as BL
-import qualified Data.Vector as V
-import Debug.Trace
-import Foreign.C.Types
-import Stats
-import Text.HTML.Scalpel hiding (URL)
+import           Data.Csv
+import           Data.Maybe                 (catMaybes, fromJust, mapMaybe)
+import           Data.UnixTime
+import qualified Data.Vector                as V
+import           Debug.Trace
+import           Foreign.C.Types
+import           Stats
+import           Text.HTML.Scalpel          hiding (URL)
 
-scrapePythonCsv :: FilePath -> IO [Maybe Stats]
-scrapePythonCsv csvFile = do
-  Right csv <- parseCsv csvFile
-  mapM scrapePythonURL $ getIssueNums csv
+scrapePythonCSV :: FilePath -> IO [Stats]
+scrapePythonCSV csvFile = do
+  Right csv <- parseCSV csvFile
+  catMaybes <$> mapM scrapePythonURL (getIssueNums csv)
 
-parseCsv :: FilePath -> IO (Either String Csv)
-parseCsv csvFile = decode NoHeader <$> BL.readFile csvFile
+parseCSV :: FilePath -> IO (Either String Csv)
+parseCSV csvFile = decode NoHeader <$> BL.readFile csvFile
 
 getIssueNums :: Csv -> [Int]
 getIssueNums = map fst . mapMaybe (BS.readInt . (V.! 1)) . V.toList
@@ -39,8 +38,7 @@ data PythonIssue = PythonIssue
 
 scrapePythonURL :: Int -> IO (Maybe Stats)
 scrapePythonURL issueNum =
-  fmap (parsePythonIssue issueNum) <$> scrapeURL url issue
-  where
+  fmap (parsePythonIssue issueNum) <$> scrapeURL url issue where
     url = "http://bugs.python.org/issue" ++ show issueNum
 
 issue :: Scraper ByteString PythonIssue
@@ -55,15 +53,17 @@ histories = chroots ("table" @: [hasClass "table-striped"] // "tr") $ texts "td"
 parsePythonIssue :: Int -> PythonIssue -> Stats
 parsePythonIssue inum issue = Stats
   { issueNum = inum
-  , period = udtSeconds $ diffUnixTime new old
+  , period = (\(CTime a) -> a) . udtSeconds $ diffUnixTime new old
   , priority = pythonFieldSets issue !! 1 !! 6
-  , reopen = pred . length . filter (BS.isInfixOf $ BS.pack "-> closed") $ map (!! 3) hists
-  }
-    where
-      hists = pythonHistories issue :: [PythonHistory]
-      time = parsePythonTime . (!! 0)
-      new = time $ head hists
-      old = time $ last hists
+  , reopen = pred . length . filter containsClosed $ map (!! 3) hists
+  } where
+    hists = pythonHistories issue :: [PythonHistory]
+    time = parsePythonTime . (!! 0)
+    new = time $ head hists
+    old = time $ last hists
+    containsClosed = BS.isInfixOf $ BS.pack "-> closed"
 
 parsePythonTime :: ByteString -> UnixTime
 parsePythonTime = parseUnixTime $ BS.pack "%Y-%m-%d&nbsp;%H:%M:%S"
+
+-- New changeset <a href="http://hg.python.org/lookup/810d70fb17a2">810d70fb17a2</a> by Serhiy Storchaka in branch 'default':
