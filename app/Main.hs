5222cf124fe02f1e56beaf2a80be6ae7e5e217b5
matonix <automatonix@gmail.com>
2016-09-27 12:57:18 +0900

    :lipstick: be configurable from command line.

--- app/Main.hs
+++ app/Main.hs
@@ -8,24 +8,24 @@ import           Stats
 import           System.Directory
 import           System.Environment     (getArgs)
 
-url = "http://bugs.python.org/issue24022"
-issue = 24022
-workingDir = "/home/maton/experimentBTS/"
-queryCsv = "query02.csv"
-statsCsv = "stats02.csv"
+-- url = "http://bugs.python.org/issue24022"
+-- issue = 24022
+-- workingDir = "/home/maton/experimentBTS/"
+-- queryCsv = "query02.csv"
+-- statsCsv = "stats02.csv"
 
 main :: IO ()
 -- main = scrapePythonIssue issue >>= printStats . maybeToList
 -- main = scrapePythonCSV queryPath >>= writeStats statsPath where
-main = scrapeWithCache queryPath scrapePythonCSV $ writeStats statsPath where
-  queryPath = workingDir ++ queryCsv
-  statsPath = workingDir ++ statsCsv
-
--- main = do
---   args <- getArgs
---   if length args /= 1
---     then putStrLn "Usage: cmd filename"
---     else do
---       let fileName = head args
---       workingDir <-  getCurrentDirectory
---       scrapeWithCache (workingDir ++ fileName) scrapePythonCSV writeStats
+-- main = scrapeWithCache queryPath scrapePythonCSV $ writeStats statsPath where
+--   queryPath = workingDir ++ queryCsv
+--   statsPath = workingDir ++ statsCsv
+main = do
+  args <- getArgs
+  if length args /= 2
+    then putStrLn "Usage: cmd queryCsv statsCsv"
+    else do
+      workingDir <-  getCurrentDirectory
+      let queryPath = workingDir ++ "/" ++ args !! 0
+      let statsPath = workingDir ++ "/" ++ args !! 1
+      scrapeWithCache queryPath scrapePythonCSV $ writeStats statsPath
