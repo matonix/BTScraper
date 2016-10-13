8da8426f7fbc0a33cb7e4fca4eeaef68783a8880
matonix <automatonix@gmail.com>
2016-10-13 14:23:56 +0900

    :sparkles: link commit # to issue # from git log

--- app/Main.hs
+++ app/Main.hs
@@ -10,39 +10,40 @@ import           Stats
 import           System.Directory
 import           System.Environment     (getArgs)
 
-commit = "1dfad5043a207e032a78ef50c3cba50488bcd300"
-prefix = "https://github.com/JodaOrg/joda-time/commits/"
--- prefix = "https://github.com/google/closure-compiler/commit/"
--- inFile = workingDir ++ "Closure-commit-db-test.csv"
--- outFile = workingDir ++ "Closure-commit-issue-db-test.csv"
--- inFile = workingDir ++ "Closure-commit-db.csv"
--- outFile = workingDir ++ "Closure-commit-issue-db.csv"
-inFile = workingDir ++ "Time-commit-db.csv"
-outFile = workingDir ++ "Time-commit-issue-db.csv"
--- issue = 2049
--- url = "https://github.com/composer/composer/issues/4967"
--- url = "https://github.com/google/closure-compiler/issues/2049"
--- url = "http://bugs.python.org/issue24022"
--- issue = 24022
 workingDir = "/home/maton/experimentBTS/"
+inFile = workingDir ++ "Lang-commit-db.csv"
+outFile = workingDir ++ "Lang-commit-issue-db.csv"
+logFile = workingDir ++ "commons-lang-log"
+main = linkGitlogCommitToIssueCSV logFile inFile outFile
+
+-- commit = "1dfad5043a207e032a78ef50c3cba50488bcd300"
+-- prefix = "https://github.com/google/closure-compiler/commit/"
+-- main = linkGithubCommitToIssue prefix commit -- test
+
+-- inFile = workingDir ++ "Time-commit-db.csv"
+-- outFile = workingDir ++ "Time-commit-issue-db.csv"
+-- prefix = "https://github.com/JodaOrg/joda-time/commits/"
+-- main = linkGithubCommitToIssueCSV prefix inFile outFile
+
+-- url = "https://github.com/google/closure-compiler/issues/2049"
+-- main = scrapeGithubURL url >>= print
+
+-- issue = 2049
+-- main = scrapeGithubIssue issue >>= printStats . maybeToList
+
+-- workingDir = "/home/maton/experimentBTS/"
 -- queryCsv = "query02.csv"
 -- statsCsv = "stats02.csv"
-
-main :: IO ()
--- main = linkGithubCommitToIssue prefix commit
-main = linkGithubCommitToIssueCSV prefix inFile outFile
--- main = scrapeGithubURL url >>= print
--- main = scrapeGithubIssue issue >>= printStats . maybeToList
--- main = scrapePythonCSV queryPath >>= writeStats statsPath where
 -- main = scrapeWithCache queryPath scrapePythonCSV $ writeStats statsPath where
 --   queryPath = workingDir ++ queryCsv
 --   statsPath = workingDir ++ statsCsv
+
 -- main = do
 --   args <- getArgs
 --   if length args /= 2
 --     then putStrLn "Usage: cmd queryCsv statsCsv"
 --     else do
 --       workingDir <-  getCurrentDirectory
---       let queryPath = workingDir ++ "/" ++ args !! 0
---       let statsPath = workingDir ++ "/" ++ args !! 1
+--       let queryPath = workingDir ++ args !! 0
+--       let statsPath = workingDir ++ args !! 1
 --       scrapeWithCache queryPath scrapePythonCSV $ writeStats statsPath
--- src/GithubCommit.hs
+++ src/GithubCommit.hs
@@ -2,7 +2,7 @@
 
 module GithubCommit
   ( linkGithubCommitToIssueCSV
-  , linkGithubCommitToIssue
+  , linkGitlogCommitToIssueCSV
   ) where
 
 import           Control.Monad
@@ -10,7 +10,10 @@ import           Csv
 import           Data.ByteString            (ByteString)
 import qualified Data.ByteString.Char8      as BS
 import qualified Data.ByteString.Lazy.Char8 as BL
+import           Data.Char                  (isDigit)
 import           Data.Csv
+import           Data.List                  (find)
+import           Data.Maybe                 (fromMaybe)
 import           Data.Vector                (Vector)
 import qualified Data.Vector                as V
 import           GHC.Generics               (Generic)
@@ -33,12 +36,18 @@ linkGithubCommitToIssueCSV prefix inCSV outCSV = do
   commitDescs <- mapM (scrapeGithubCommit prefix) $ toNewCommitIds csv
   writeCSV outCSV $ zipWith (\c i -> c {issueId = i}) (V.toList csv) $ map parseIssueId commitDescs
 
+linkGitlogCommitToIssueCSV :: FilePath -> FilePath -> FilePath -> IO ()
+linkGitlogCommitToIssueCSV logFile inCSV outCSV = do
+  Right csv <- readCSV inCSV
+  logs <- map BS.words . BS.lines <$> BS.readFile logFile
+  let issueIds = map (parseGitLog logs) . map newCommitId . V.toList $ csv
+  writeCSV outCSV $ zipWith (\c i -> c {issueId = i}) (V.toList csv) issueIds
+
 -- for test
-linkGithubCommitToIssue :: String -> String -> IO ()
-linkGithubCommitToIssue prefix commitId = do
-  commitDescs <- scrapeGithubCommit prefix commitId
-  print $ parseIssueId commitDescs
-  -- print commitDescs
+-- linkGithubCommitToIssue :: String -> String -> IO ()
+-- linkGithubCommitToIssue prefix commitId = do
+--   commitDescs <- scrapeGithubCommit prefix commitId
+--   print $ parseIssueId commitDescs
 
 toNewCommitIds :: Vector CommitIssue -> [String]
 toNewCommitIds = map (BS.unpack . newCommitId) . V.toList
@@ -51,10 +60,30 @@ commit = BS.append <$> commitTitle <*> commitDesc where
   commitTitle = text ("p" @: [hasClass "commit-title"])
   commitDesc = text ("div" @: [hasClass "commit-desc"])
 
+-- for Commons-lang
+parseGitLog :: [[ByteString]] -> ByteString -> ByteString
+parseGitLog logs commitId = pickIssueId . find lang . fromMaybe [] $ find (\a -> head a == commitId) logs where
+  lang = BS.isInfixOf (BS.pack "LANG-")
+  pickIssueId a = readIntEmpty . BS.filter isDigit $ maybeEmpty a
+
+-- for Joda-time
 parseIssueId :: Maybe ByteString -> ByteString
-parseIssueId (Just str) = pickIssueId . dropWhile notFoundIssue $ mywords str where
+parseIssueId str = pickIssueId . find sharp . mywords $ maybeEmpty str where
   mywords = BS.splitWith (flip elem "\n\r\t ")
-  notFoundIssue str = str /= (BS.pack "issue") && str /= (BS.pack "Issue")
-  pickIssueId (_:a:_) = maybe BS.empty (BS.pack . show . fst) $ BS.readInt a
-  pickIssueId _ = BS.empty
-parseIssueId Nothing = BS.empty
+  sharp str = BS.length str >= 2 && BS.head str == '#'
+  pickIssueId a = readIntEmpty . BS.tail $ maybeEmpty a
+
+-- for Closure-compiler
+-- parseIssueId :: Maybe ByteString -> ByteString
+-- parseIssueId (Just str) = pickIssueId . dropWhile notFoundIssue $ mywords str where
+--   mywords = BS.splitWith (flip elem "\n\r\t ")
+--   notFoundIssue str = str /= (BS.pack "issue") && str /= (BS.pack "Issue")
+--   pickIssueId (_:a:_) = maybe BS.empty (BS.pack . show . fst) $ BS.readInt a
+--   pickIssueId _ = BS.empty
+-- parseIssueId Nothing = BS.empty
+
+maybeEmpty :: Maybe ByteString -> ByteString
+maybeEmpty = fromMaybe BS.empty
+
+readIntEmpty :: ByteString -> ByteString
+readIntEmpty = maybe BS.empty (BS.pack . show . fst) . BS.readInt
